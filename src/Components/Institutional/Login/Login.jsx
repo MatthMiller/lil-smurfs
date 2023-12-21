3fd9258850041ea9eb6f144217fa59a1550f5f57
MatthMiller <matheus.monteiro.genesis@gmail.com>
2023-12-21 15:54:33 -0300

    Login

--- src/Components/Institutional/Login/Login.jsx
+++ src/Components/Institutional/Login/Login.jsx
@@ -4,9 +4,11 @@ import bgPattern from '../../../Images/Login/bgPattern.svg';
 import formStyle from '../../Common/Form.module.css';
 import VisibilityOffIcon from '../../../Images/Common/Icons/VisibilityOffIcon';
 import VisibilityOnIcon from '../../../Images/Common/Icons/VisibilityOnIcon';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [passwordVisibility, setPasswordVisibility] = React.useState(false);
+  const [rememberMe, setRememberMe] = React.useState(false);
 
   return (
     <div className={style.bg} style={{ background: `url(${bgPattern})` }}>
@@ -18,7 +20,7 @@ const Login = () => {
               <h3 className={style.subtitle}>Área do usuário</h3>
             </div>
 
-            <form className={style.login}>
+            <div className={style.login}>
               <div className={style.inputContainer}>
                 <label htmlFor='email' className={formStyle.label}>
                   E-mail
@@ -38,7 +40,7 @@ const Login = () => {
                 <div className={formStyle.passwordContainer}>
                   <input
                     className={formStyle.input}
-                    type={passwordVisibility ? 'password' : 'text'}
+                    type={passwordVisibility ? 'text' : 'password'}
                     name='password'
                     id='password'
                     placeholder='Insira aqui sua senha'
@@ -56,7 +58,32 @@ const Login = () => {
                   </label>
                 </div>
               </div>
-            </form>
+
+              <div
+                className={
+                  rememberMe
+                    ? `${formStyle.buttonCheckboxContainer} ${formStyle.buttonChecked}`
+                    : formStyle.buttonCheckboxContainer
+                }
+                onClick={() => setRememberMe(!rememberMe)}
+              >
+                <button className={formStyle.buttonCheckbox}></button>
+                <p className={formStyle.checkboxText}>Lembrar login</p>
+              </div>
+
+              <div className={style.loginBottom}>
+                <Link className={formStyle.forgotPassword}>
+                  Esqueci minha senha
+                </Link>
+                <Link className={formStyle.filledButton}>Entrar</Link>
+                <div className={formStyle.divisorContainer}>
+                  <div className={formStyle.divisorLine}></div>
+                  <p className={formStyle.hintText}>Não possui conta?</p>
+                  <div className={formStyle.divisorLine}></div>
+                </div>
+                <Link className={formStyle.ghostButton}>Cadastre-se</Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
